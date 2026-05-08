e33a8c879699b538e5ef6a02e550af12e2ac466f
Alexander Waite <a.waite1703@gmail.com>
2026-05-07 23:34:41 -0500

    fixxing uploads issue

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 def _engine_options():
     db_url = os.getenv("DATABASE_URL", "")
@@ -29,4 +29,5 @@ class Config:
         "INFO3180_GROUP_PROJECT_SECRET_KEY"
     )
 
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads")
+
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, "..", "uploads")
